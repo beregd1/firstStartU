b806cae0ac67814b7b9376ba1b4f8209de2fa368
beregd1 <119509393+beregd1@users.noreply.github.com>
2022-11-30 16:26:42 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        String hello = "hello";
+        String name = "My name is Dima";
+        int age = 28;
 
-        System.out.println("Hello world!");
+        for (int i = 0; i < age; i++) {
+            System.out.println(hello);
+            System.out.println(name);
+        }
     }
 }
